b7a8fcd3567083fae20670b522d1ad0421945eaf
Pratik <63846928+iamstarstuff@users.noreply.github.com>
2023-07-13 12:35:21 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,10 @@
 #
 import os
 import sys
-#sys.path.insert(0, os.path.abspath('../maglimit'))
-#sys.path.insert(0, os.path.abspath('./../maglimit'))
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath('../maglimit'))
+#sys.path.insert(0, os.path.abspath('./../maglimit'))
+
 
 
 # -- Project information -----------------------------------------------------
